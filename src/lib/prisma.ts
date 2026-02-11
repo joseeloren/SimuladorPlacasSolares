e5608fa739b6b6c64b9d621478bdeba5d3586ffe
Jose Lorenzo <jose.lorenzo117@alu.ulpgc.es>
2026-02-11 09:27:29 +0000

    Cambios

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
 
-const adapter = new PrismaLibSql({
+import { createClient } from '@libsql/client';
+
+const libsql = createClient({
   url: process.env.TURSO_DATABASE_URL || process.env.DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
+
+const adapter = new PrismaLibSql(libsql);
 
 const prismaClientSingleton = () => {
   return new PrismaClient({ adapter });
